cd4d59369ffb39dcbf55109045dc0310ce5b6fc6
Enkratia <manofthetreasure@gmail.com>
2024-04-16 23:58:34 +0300

    m

--- src/components/custom/index.tsx
+++ src/components/custom/index.tsx
@@ -11,15 +11,72 @@ import cs from "../../scss/helpers.module.scss";
 import s from "./custom.module.scss";
 import Icon from "../../../public/img/default/check.svg";
 
+const url =
+  "https://videocdn.cdnpk.net/joy/content/video/free/2022-01/large_preview/220114_01_Drone_4k_017.mp4";
+
 export const Custom: React.FC = () => {
+  const videoRef = React.useRef<HTMLVideoElement>(null);
+  const progressRef = React.useRef<HTMLProgressElement>(null);
+
+  const [test, setState] = React.useState({});
+
+  const [videoOptions, setVideoOptions] = React.useState({});
+
   const [domLoaded, setDomLoaded] = React.useState(false);
 
   React.useEffect(() => {
     setDomLoaded(true);
   }, []);
 
-  const url =
-    "https://videocdn.cdnpk.net/joy/content/video/free/2022-01/large_preview/220114_01_Drone_4k_017.mp4";
+  // **
+  const onVideoLoadedMetadata = () => {
+    const video = videoRef.current;
+    const progress = progressRef.current;
+    if (!video || !progress) return;
+
+    progress.setAttribute("max", video.duration.toString());
+  };
+
+  const onPlayPauseUp = () => {
+    const video = videoRef.current;
+    if (!video) return;
+
+    if (video.paused || video.ended) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  };
+
+  const onStopUp = () => {
+    const video = videoRef.current;
+    const progress = progressRef.current;
+    if (!video || !progress) return;
+
+    video.pause();
+    video.currentTime = 0;
+    progress.value = 0;
+  };
+
+  const onMuteUp = () => {
+    const video = videoRef.current;
+    if (!video) return;
+
+    video.muted = !video.muted;
+  };
+
+  // **
+  const onVolumeUp = (dir: string) => {
+    const video = videoRef.current;
+    if (!video) return;
+
+    const currentVolume = Math.floor(video.volume * 10) / 10;
+    if (dir === "+" && currentVolume < 1) {
+      video.volume += 0.1;
+    } else if (dir === "-" && currentVolume > 0) {
+      video.volume -= 0.1;
+    }
+  };
 
   return (
     <section className={s.root}>
@@ -28,65 +85,68 @@ export const Custom: React.FC = () => {
       <div className={`${s.container} ${cs.container}`}>
         <div className={s.videoWrapper}>
           <div className={s.videoWrapperInner}>
-            {/* {domLoaded ? (
-              <ReactPlayer
-                url={url}
-                className={s.video}
-                controls={true}
-                width="100%"
-                height="100%"
-                playIcon={<Icon width="50px" height="50px" />}
-              />
-            ) : (
-              ""
-            )} */}
-            <figure id="videoContainer">
-              <video id="video" controls preload="metadata" poster="img/poster.jpg">
-                <source src={url} type="video/mp4" />
-              </video>
+            <video
+              ref={videoRef}
+              onLoadedMetadata={onVideoLoadedMetadata}
+              controls={false}
+              preload="metadata"
+              // poster="img/poster.jpg"
+              className={s.video}>
+              <source src={url} type="video/mp4" />
+            </video>
 
-              <ul id="video-controls" className="controls">
-                <li>
-                  <button id="playpause" type="button">
+            <div className={s.controls}>
+              <div className={s.progressWrapper}>
+                <progress ref={progressRef} className={s.progress} value="0"></progress>
+              </div>
+
+              <ul className={s.btns}>
+                <li className={`${s.item} ${s.playpauseWrapper}`}>
+                  <button
+                    onPointerUp={onPlayPauseUp}
+                    className={`${s.btn} ${s.playpause}`}
+                    type="button">
                     Play/Pause
                   </button>
                 </li>
-                <li>
-                  <button id="stop" type="button">
+
+                <li className={s.item}>
+                  <button onPointerUp={onStopUp} className={`${s.btn} ${s.stop}`} type="button">
                     Stop
                   </button>
                 </li>
-                <li className="progress">
-                  <progress id="progress" value="0">
-                    <span id="progress-bar"></span>
-                  </progress>
-                </li>
-                <li>
-                  <button id="mute" type="button">
+
+                <li className={s.item}>
+                  <button onPointerUp={onMuteUp} className={`${s.btn} ${s.mute}`} type="button">
                     Mute/Unmute
                   </button>
                 </li>
-                <li>
-                  <button id="volinc" type="button">
+
+                <li className={s.item}>
+                  <button
+                    onPointerUp={() => onVolumeUp("+")}
+                    className={`${s.btn} ${s.volumeUp}`}
+                    type="button">
                     Vol+
                   </button>
                 </li>
-                <li>
-                  <button id="voldec" type="button">
+
+                <li className={s.item}>
+                  <button
+                    onPointerUp={() => onVolumeUp("-")}
+                    className={`${s.btn} ${s.volumeDown}`}
+                    type="button">
                     Vol-
                   </button>
                 </li>
-                <li>
-                  <button id="fs" type="button">
+
+                <li className={s.item}>
+                  <button className={`${s.btn} ${s.fullscreen}`} type="button">
                     Fullscreen
                   </button>
                 </li>
               </ul>
-
-              <figcaption>
-                &copy; Blender Foundation |<a href="http://mango.blender.org">mango.blender.org</a>
-              </figcaption>
-            </figure>
+            </div>
 
             {/* <video
               className={s.video}
@@ -99,6 +159,19 @@ export const Custom: React.FC = () => {
     </section>
   );
 };
+
+/* {domLoaded ? (
+  <ReactPlayer
+    url={url}
+    className={s.video}
+    controls={true}
+    width="100%"
+    height="100%"
+    playIcon={<Icon width="50px" height="50px" />}
+  />
+) : (
+  ""
+)} */
 
 ////////////////////////////////////////////////////////////////
 /* <div className={s.videoWrapper}>
